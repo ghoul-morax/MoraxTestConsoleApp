018edfd13bfdfdc9276b0b0f110f89ae5d675b02
ghoul-morax <118064323+ghoul-morax@users.noreply.github.com>
2022-12-05 20:35:54 +0300

    add welcome

--- MoraxTestConsoleApp/Program.cs
+++ MoraxTestConsoleApp/Program.cs
@@ -10,6 +10,8 @@ namespace MoraxTestConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            Console.ReadLine();
         }
     }
 }
